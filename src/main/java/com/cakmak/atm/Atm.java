b8dcf1e902a147d973f90f7928b6a7e30c2221c6
KarakusAlican <tr.alikarakus2008@gmail.com>
2023-11-03 20:26:00 +0300

    [FATMACAKMAK] Atm ve AtmMain classlari duzeltildi.

--- src/main/java/com/cakmak/atm/Atm.java
+++ src/main/java/com/cakmak/atm/Atm.java
@@ -1,5 +1,31 @@
 package com.cakmak.atm;
 
+import java.util.Scanner;
+
 public class Atm {
            public Integer bakiyem;
+    Scanner scan = new Scanner(System.in);
+    public void paracekVeyaParaGonder(){
+        System.out.println("para miktarı girin:");
+        int para= scan.nextInt();
+        if (para> bakiyem){
+            System.out.println("Bakiye yetersiz!");
+        } else {
+            bakiyem = bakiyem-para;
+            System.out.println("yeni bakiyeniz:"+bakiyem);
+        }
+    }
+    
+    public void bakiyeOgrenme(){
+        System.out.println("Bakiyeniz: "+bakiyem);
+        System.out.println("");
+    }
+    
+    public void islemler(){
+        System.out.println("1-para çek");
+        System.out.println("2-bakiye öğren");
+        System.out.println("3-para gönder");
+        System.out.println("4-cıkıs yap");
+        System.out.print("Yapmak istediğiniz işlemi seçiniz:");
+    }
 }
